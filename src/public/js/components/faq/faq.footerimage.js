21cf8369b0bec128b4ba3e6290dfc120561bc37e
Vinod Mukati <vinod.mukati@indegene.com>
2017-06-13 19:37:25 +0530

    code changes

--- src/public/js/components/faq/faq.footerimage.js
+++ src/public/js/components/faq/faq.footerimage.js
@@ -2,30 +2,35 @@ import React from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router";
 
-let HtmlToReactParser = require('html-to-react').Parser;
-let htmlToReactParser = new HtmlToReactParser();
-
 class FaqFooterImage extends React.Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        let footerImage = '', imageButton = '';
-        if(this.props.faq.bannerContent.footerBanner)
-            this.props.faq.bannerContent.footerBanner.map(function(item) {                
-                if(item.buttonLinks)
-                    item.buttonLinks.forEach(function(butn) {
-                        imageButton = imageButton + '<a title="' + butn.hyperlinkText + '" href="' + butn.hyperlinkUrl + '" className="button">' + butn.hyperlinkText + '</a>';
-                    });
-                footerImage = '<div className="contact" style="background-image: url(' + item.imagePath +')"><div className="content"><p className="txt">' + item.description + '</p><br/>'+ imageButton +'</div></div>';
-            });
+        const {faq} = this.props;
 
-        const reactElement = htmlToReactParser.parse(footerImage);
+        let footerImage = faq.bannerContent.footerBanner && faq.bannerContent.footerBanner.map(function(item, i) {                
+                let url = 'url(' + item.imagePath + ')';
+                let imageButton = item.buttonLinks && item.buttonLinks.map(function(btn, i) {
+                    return (
+                        <Link title={btn.hyperlinkText} to={btn.hyperlinkUrl} className="button" key={i}>{btn.hyperlinkText}</Link>
+                    )
+                });
+
+                return (
+                    <div className="contact" style={{backgroundImage: url}} key={i}>
+                        <div className="content">
+                            <p className="txt">{item.description}</p><br/>
+                            {imageButton}
+                        </div>
+                    </div>
+                )
+            });
         
         return (
             <div>
-                {reactElement}
+                {footerImage}
             </div>
         );
     }

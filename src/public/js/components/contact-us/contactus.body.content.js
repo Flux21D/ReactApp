2a1e5f4491cdde401fc0ba54d86bed040816af52
Vinod Mukati <vinod.mukati@indegene.com>
2017-07-05 18:09:41 +0530

    Fixed issue #54557: [Contacta con nosotros]My Mentor link is not hyperlinked

--- src/public/js/components/contact-us/contactus.body.content.js
+++ src/public/js/components/contact-us/contactus.body.content.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router";
+import marked from "marked";
 
 let HtmlToReactParser = require('html-to-react').Parser;
 let htmlToReactParser = new HtmlToReactParser();
@@ -8,6 +9,28 @@ let htmlToReactParser = new HtmlToReactParser();
 class ContactBodyContent extends React.Component {
     constructor(props) {
         super(props);
+
+        this.clickHandler = this.clickHandler.bind(this);
+    }
+
+    componentDidMount() {
+        marked.setOptions({
+          renderer: new marked.Renderer(),
+          gfm: true,
+          tables: true,
+          breaks: false,
+          pedantic: false,
+          sanitize: true,
+          smartLists: true,
+          smartypants: false
+        });
+    }
+
+    clickHandler(eve) {
+        if(eve.target.tagName.toLowerCase() === 'a') {
+            window.open(eve.target.getAttribute("href"), '_blank');
+        }
+        eve.preventDefault();
     }
 
     render() {
@@ -17,8 +40,9 @@ class ContactBodyContent extends React.Component {
             contentLeft = contentLeft + '<h2 className="title-big nmt">' + item.someShortText + '</h2><p>' + item.someLongText+ '</p>';
         });
         this.props.contactus.bodyContentRight.map(function(item) {
-            contentRight = contentRight + '<h2 className="title-big nmt">' + item.someShortText + '</h2><p>' + item.someLongText + '</p>';
+            contentRight = contentRight + '<h2 className="title-big nmt">' + item.someShortText + '</h2><p>' + marked(item.someLongText) + '</p>';
         });
+        
         const leftElement = htmlToReactParser.parse(contentLeft);
         const rightElement = htmlToReactParser.parse(contentRight);
 
@@ -30,7 +54,7 @@ class ContactBodyContent extends React.Component {
                         <p className="al-right"><a title="Realizar consulta" className="button" href="mailto:ejemplo@ejemplo.com"><span className="aw"><img className="svg svgW  " src="img/icons/envelope.svg" title="Icono"/></span> Realizar consulta</a></p>
                     </div>
                 </div>
-                <div className="col-right">
+                <div className="col-right" onClick={this.clickHandler}>
                     <div className="half-content">
                         {rightElement}
                     </div>
